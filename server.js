459e443ded27bac47a94ea264370f2aa46ab15af
T3DDY <142236763+T3DDYHD@users.noreply.github.com>
2025-01-15 21:49:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,33 +7,26 @@ const port = 3000;
 
 const viewersFilePath = path.join(__dirname, 'viewers.json');
 
-// Set to store temporary viewers
 const viewersSet = new Set();
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-// Endpoint to get viewer count
 app.get('/get-viewer-count', (req, res) => {
-    // Return the size of the set to get the unique viewer count
     res.json({ count: viewersSet.size });
 });
 
-// Endpoint to update viewer count
 app.post('/update-viewer-count', (req, res) => {
-    const ip = req.body.ip;  // Use IP to track temporary uniqueness
+    const ip = req.body.ip; 
 
-    // Check if the IP has already been counted
     if (!viewersSet.has(ip)) {
-        viewersSet.add(ip);  // Add IP to the set
+        viewersSet.add(ip);
         res.status(200).json({ success: true });
     } else {
-        res.status(200).json({ success: true });  // IP has already been counted
+        res.status(200).json({ success: true });
     }
 });
 
-// Endpoint to get the current config
 app.get('/get-config', (req, res) => {
     fs.readFile(configFilePath, 'utf8', (err, data) => {
         if (err) {
